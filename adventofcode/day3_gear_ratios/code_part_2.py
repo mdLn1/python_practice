8593f21f9002414e9ad0b7bdfb829646919d923e
mdln1 <madalinc.preda@gmail.com>
2023-12-04 22:21:19 +0100

    started day 4

--- adventofcode/day3_gear_ratios/code_part_2.py
+++ adventofcode/day3_gear_ratios/code_part_2.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 
 part_one_invalid_neighbors = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]
+part_two_valid_number_coordinates = []
+
 
 def get_directions(x, y):
     top_left = (x - 1, y - 1)
@@ -13,22 +15,12 @@ def get_directions(x, y):
     left = (x - 1, y)
     return [top_left, top, top_right, right, bottom_right, bottom, bottom_left, left]
 
+
 def is_valid_part_number(schematic: dict, coordinates: tuple) -> bool:
     for direction in get_directions(coordinates[0], coordinates[1]):
         if schematic[direction] not in part_one_invalid_neighbors:
             return True
     return False
-
-
-def create_schematic_dict(problem_input: list[str]) -> dict:
-    schematic_dict = defaultdict(str)
-    for y_coord, line in enumerate(problem_input):
-        for x_coord, char in enumerate(line):
-            schematic_dict[(x_coord, y_coord)] = char
-    return schematic_dict
-
-
-part_two_valid_number_coordinates = []
 
 
 def create_number(numbers_and_coordinates: list) -> int:
@@ -41,14 +33,14 @@ def create_number(numbers_and_coordinates: list) -> int:
     return int(number)
 
 
-def get_part_number_list(schematic: dict, columns: int, rows: int) -> list[int]:
+def get_part_number_list(schematic: dict, rows: int, columns: int) -> list[int]:
     valid_part_numbers = []
     potential_part_numbers = []
     for i in range(rows):
         number_collector = []
         for j in range(columns + 1):
-            if schematic[(j, i)] in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
-                number_collector.append(((j, i), schematic[j, i]))
+            if schematic[(i, j)] in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
+                number_collector.append(((i, j), schematic[i, j]))
             else:
                 potential_part_numbers.append(number_collector.copy())
                 number_collector.clear()
@@ -61,13 +53,14 @@ def get_part_number_list(schematic: dict, columns: int, rows: int) -> list[int]:
     return valid_part_numbers
 
 
-def get_asterisk_coordinates(schematic: dict, columns: int, rows: int) -> list[tuple]:
+def get_asterisk_coordinates(schematic: dict, rows: int, columns: int) -> list[tuple]:
     asterisk_coordinates = []
     for i in range(rows):
         for j in range(columns + 1):
-            if schematic[(j, i)] == "*":
-                asterisk_coordinates.append((j, i))
+            if schematic[(i, j)] == "*":
+                asterisk_coordinates.append((i, j))
     return asterisk_coordinates
+
 
 def get_gear_ratios(asterisks: list) -> list[int]:
     gear_ratios = []
@@ -96,9 +89,9 @@ if __name__ == '__main__':
     data = defaultdict(str)
     for i, line in enumerate(lines):
         for j, char in enumerate(line):
-            data[(j, i)] = char
+            data[((i, j))] = char
 
-    part_numbers = get_part_number_list(data, len(lines[0]), len(lines))
-    asterisk_locations = get_asterisk_coordinates(data, len(lines[0]), len(lines))
+    part_numbers = get_part_number_list(data, len(lines), len(lines[0]))
+    asterisk_locations = get_asterisk_coordinates(data, len(lines), len(lines[0]))
     the_gear_ratios = get_gear_ratios(asterisk_locations)
     print(f"Sum of gear ratios is {sum(the_gear_ratios)}")
